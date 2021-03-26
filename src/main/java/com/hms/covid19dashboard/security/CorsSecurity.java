0511ccb327a08ddf3e0bb38e4c5e7770d20fcf16
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-03-26 17:19:16 +0530

    Error in webservice resolved

--- src/main/java/com/hms/covid19dashboard/security/CorsSecurity.java
+++ src/main/java/com/hms/covid19dashboard/security/CorsSecurity.java
@@ -1,7 +1,9 @@
 package com.hms.covid19dashboard.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,6 +12,8 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
+@EnableWebSecurity
+@Configuration
 public class CorsSecurity extends WebSecurityConfigurerAdapter{
 
      @Override
